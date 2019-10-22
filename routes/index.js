25ede42535d8b64771400d089663bcf61809cc46
vihangmirkhelkar <vihang10@csu.fullerton.edu>
2019-10-22 16:20:21 -0700

    Add Unit tests using mocha and chai to verify register, login and webhook endpoints

--- routes/index.js
+++ routes/index.js
@@ -7,36 +7,36 @@ const errorHandler = require('../utils/error');
 const router = Router();
 
 router.post('/register', (req, res,next) => {
-  if(!req.body.username || !req.body.password) {
+  if (!req.body.username || !req.body.password) {
     throw new errorHandler.missingCredentials('Missing credentials to register please add username and password');
   }
-  if(handleUser.checkUser(req.body.username)) {
+  if (handleUser.checkUser(req.body.username)) {
     throw new errorHandler.userNameExists('Username already exists please pick another username');
   }
 
   encrypt(req.body.password)
-      .then(encryptedPassword => {
-        handleUser.storeUserInformation({username: req.body.username,
-          password: encryptedPassword});
-        return tokenUtil.generateToken(req.body.username)
-            .then(token => {
-              res.status(200).send({token: token});
-            })
-            .catch(e => {
-              next(e);
-            });
-      })
-      .catch(e => {
-        next(e);
-      })
+    .then(encryptedPassword => {
+      handleUser.storeUserInformation({ username: req.body.username,
+        password: encryptedPassword });
+      return tokenUtil.generateToken(req.body.username)
+        .then(token => {
+          res.status(200).send({ token });
+        })
+        .catch(e => {
+          next(e);
+        });
+    })
+    .catch(e => {
+      next(e);
+    });
 });
 
 router.post('/login', (req, res, next) => {
   const userDetails = req.body;
-  if(!userDetails.username || !userDetails.password) throw new errorHandler.missingCredentials('Missing credentials please enter username and password');
-  if(!handleUser.checkUser(userDetails.username)) throw new errorHandler.usernameNotFound('Username does not exists');
+  if (!userDetails.username || !userDetails.password) throw new errorHandler.missingCredentials('Missing credentials please enter username and password');
+  if (!handleUser.checkUser(userDetails.username)) throw new errorHandler.usernameNotFound('Username does not exists');
   const userInformation = handleUser.getUserInformation(userDetails.username);
-  
+
   verifyPassword(userDetails.password, userInformation.password)
       .then(verifiedPassword => {
         if(!verifiedPassword) throw new errorHandler.invalidPassword('Invalid Password please check your password');
--- test/app-test.js
+++ test/app-test.js
@@ -5,23 +5,137 @@ const app = require('../app');
 
 chai.should();
 chai.use(chaiHttp);
+const { expect } = chai;
+let authToken;
 
 /* Test the /GET route */
-describe('app index route', () => {
-  it('it should GET /', (done) => {
+// describe('app index route', () => {
+//   it('it should GET /', (done) => {
+//     chai.request(app)
+//       .get('/')
+//       .end((err, res) => {
+//         res.should.have.status(200);
+//         done();
+//       });
+//   });
+//
+//   it('it should handle 404 error', (done) => {
+//     chai.request(app)
+//       .get('/notExist')
+//       .end((err, res) => {
+//         res.should.have.status(404);
+//         done();
+//       });
+//   });
+// });
+
+describe('register new user', () => {
+  it('should register a new user', (done) => {
     chai.request(app)
-      .get('/')
+      .post('/register')
+      .send({ username: 'vihang', password: 'vihangmi' })
       .end((err, res) => {
         res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('token');
+        authToken = res.body.token;
         done();
       });
   });
-
-  it('it should handle 404 error', (done) => {
+  it('should handle missing username', (done) => {
     chai.request(app)
-      .get('/notExist')
+      .post('/register')
+      .send({ password: 'vihangmi' })
       .end((err, res) => {
-        res.should.have.status(404);
+        res.should.have.status(401);
+        expect('Content-Type', '/html/');
+        expect(res).to.be.a('object', 'Missing credentials to register please add username and password');
+        done();
+      });
+  });
+  it('should handle missing password', (done) => {
+    chai.request(app)
+      .post('/register')
+      .send({ username: 'vihang' })
+      .end((err, res) => {
+        res.should.have.status(401);
+        expect('Content-Type', '/html/');
+        expect(res).to.be.a('object', 'Missing credentials to register please add username and password');
+        done();
+      });
+  });
+});
+
+describe('login user', () => {
+  it('should login user', (done) => {
+    chai.request(app)
+      .post('/login')
+      .send({ username: 'vihang', password: 'vihangmi' })
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.json;
+        res.body.should.be.a('object');
+        res.body.should.have.property('token');
+        authToken = res.body.token;
+        done();
+      });
+  });
+  it('should handle missing username', (done) => {
+    chai.request(app)
+      .post('/login')
+      .send({ password: 'vihangmi' })
+      .end((err, res) => {
+        res.should.have.status(401);
+        expect('Content-Type', '/html/');
+        expect(res).to.be.a('object', 'Missing credentials to register please add username and password');
+        done();
+      });
+  });
+  it('should handle missing password', (done) => {
+    chai.request(app)
+      .post('/login')
+      .send({ username: 'vihang' })
+      .end((err, res) => {
+        res.should.have.status(401);
+        expect('Content-Type', '/html/');
+        expect(res).to.be.a('object', 'Missing credentials to register please add username and password');
+        done();
+      });
+  });
+  it('should handle incorrect password', (done) => {
+    chai.request(app)
+      .post('/login')
+      .send({ username: 'vihang', password: 'someOtherPassword' })
+      .end((err, res) => {
+        res.should.have.status(401);
+        expect('Content-Type', '/html/');
+        expect(res).to.be.a('object', 'Invalid Password please check your password');
+        done();
+      });
+  });
+  it('should handle case where user is not registered', (done) => {
+    chai.request(app)
+      .post('/login')
+      .send({ username: 'differentUser', password: 'anyPassowrd' })
+      .end((err, res) => {
+        res.should.have.status(403);
+        expect('Content-Type', '/html/');
+        expect(res).to.be.a('object', 'Username does not exists');
+        done();
+      });
+  });
+});
+
+describe('Third party Api should connect with webhook', () => {
+  it('should be able to successfully connect', (done) => {
+    chai.request(app)
+      .post('/getStocksHook')
+      .send({ ticker: 'GOOG', price: 240.67 })
+      .end((err, res) => {
+        res.should.have.status(200);
+        expect('Content-Type', '/html/');
+        expect(res).to.be.a('object', 'OK');
         done();
       });
   });
